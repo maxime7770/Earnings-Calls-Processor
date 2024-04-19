d2b16afa6c67f6495093fb648e29e0bb5537abe8
maxime7770 <maxwolf34@gmail.com>
2024-04-19 17:48:00 -0400

    final dataframe updated

--- archives/test.py
+++ archives/test.py
@@ -1,9 +1,10 @@
 import src.sentiment_analysis as sentiment_analysis
 import pandas as pd
 
-# df = pd.read_csv('OutputData/output.csv')
+# df = pd.read_csv('OutputData/sentence_level_output.csv')
 
 # df_sentiment = sentiment_analysis.get_sentiment_bert(df)
+# df_sentiment.to_csv('OutputData/sentiment_sentence_level.csv')
 # df_sentiment = sentiment_analysis.get_sentiment_topic(df_sentiment)
 # df_sentiment = sentiment_analysis.aggregate_sentiment(df_sentiment)
 
@@ -21,7 +22,7 @@ import pandas as pd
 
 
 
-import pandas as pd
+# import pandas as pd
 
 
 df1 = pd.read_csv('OutputData/sentiment_features.csv')
@@ -33,14 +34,11 @@ df4.rename(columns={'company_1': 'company_name', 'date_1': 'date'}, inplace=True
 for i in range(2, 16):
     df4 = df4.drop(['company_' + str(i), 'date_' + str(i)], axis=1)
 
-df1.drop(['Unnamed: 0'], axis=1, inplace=True)
-
 
 # merge all dataframes
 df = pd.merge(df1, df2, on=['company_name', 'date'], how='inner')
 df = pd.merge(df, df3, on=['company_name', 'date'], how='inner')
 df = pd.merge(df, df4, on=['company_name', 'date'], how='inner')
 
-df.drop(['Unnamed: 0.1'], axis=1, inplace=True)
 
 df.to_csv('OutputData/final_output.csv')

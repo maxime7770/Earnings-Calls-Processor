387b4df8bb572c877d0e3d9e3a62cddf2f7d8313
maxime7770 <maxwolf34@gmail.com>
2024-04-27 19:00:00 -0400

    last update - data and eda

--- archives/test.py
+++ archives/test.py
@@ -25,26 +25,27 @@ import pandas as pd
 # import pandas as pd
 
 
-df1 = pd.read_csv('OutputData/sentiment_features.csv')
-df4 = pd.read_csv('OutputData/features_audio.csv')
-df2 = pd.read_csv('OutputData/readability.csv')
-df3 = pd.read_csv('OutputData/attribute_and_similarity.csv')
+# df1 = pd.read_csv('OutputData/sentiment_features.csv')
+# df4 = pd.read_csv('OutputData/features_audio.csv')
+# df2 = pd.read_csv('OutputData/readability.csv')
+# df3 = pd.read_csv('OutputData/attribute_and_similarity.csv')
 
-df4.rename(columns={'company_1': 'company_name', 'date_1': 'date'}, inplace=True)
-for i in range(2, 16):
-    df4 = df4.drop(['company_' + str(i), 'date_' + str(i)], axis=1)
+# df4.rename(columns={'company_1': 'company_name', 'date_1': 'date'}, inplace=True)
+# for i in range(2, 16):
+#     df4 = df4.drop(['company_' + str(i), 'date_' + str(i)], axis=1)
 
 
-# merge all dataframes
-df = pd.merge(df1, df2, on=['company_name', 'date'], how='inner')
-df = pd.merge(df, df3, on=['company_name', 'date'], how='inner')
-df = pd.merge(df, df4, on=['company_name', 'date'], how='inner')
+# # merge all dataframes
+# df = pd.merge(df1, df2, on=['company_name', 'date'], how='inner')
+# df = pd.merge(df, df3, on=['company_name', 'date'], how='inner')
+# df = pd.merge(df, df4, on=['company_name', 'date'], how='inner')
 
-for col in df.columns:
-    if 'Unnamed' in col:
-        df.drop([col], axis=1, inplace=True)
+# for col in df.columns:
+#     if 'Unnamed' in col:
+#         df.drop([col], axis=1, inplace=True)
 
-df.to_csv('OutputData/final_output.csv')
+# df.to_csv('OutputData/final_output.csv')
 
-df = pd.read_csv('OutputData/final_output.csv')
-print(len(df.columns))
+
+df = pd.read_csv('sentiment_sentence_level.csv')
+print(len(df['speaker'].unique()))
